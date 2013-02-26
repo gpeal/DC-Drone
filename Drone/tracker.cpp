62f6322a9a5e7a54a3b2bcbfe735272f776fb745
Gabriel Peal <gpeal@u.northwestern.edu>
2013-02-26 14:04:41 -0600

    FEATURE: vast improvements to the tracking algo

--- Drone/tracker.cpp
+++ Drone/tracker.cpp
@@ -8,16 +8,19 @@ Tracker::Tracker(int _edge_mode, int _edge, int _laser_pin, int _transistor_pin)
     laser_pin(_laser_pin),
     transistor_pin(_transistor_pin)
 {
-
-  servo = new Servo();
-  servo->attach(9);
+  left_sensor = new Sensor();
+  left_sensor->servo = new Servo();
+  left_sensor->servo->attach(9);
+  left_sensor->servo_pos = 90;
+  left_sensor->servo->write(left_sensor->servo_pos);
+  left_sensor->servo_direction = !edge;
+  left_sensor->last_reading = 0;
+  reading_threshold = 40;
   debug->log("Attaching servo to port 9");
 
   laser_timer = new Timer();
   pinMode(laser_pin, OUTPUT);
-  // laser_timer->oscillate(laser_pin, 100, LOW);
-  laser_timer->every(50, this, toggle_laser_wrapper);
-  last_transistor_reading = 0;
+  laser_timer->every(50, this, execute_wrapper);
 }
 
 void Tracker::loop(void)
@@ -25,42 +28,90 @@ void Tracker::loop(void)
   laser_timer->update();
 }
 
-// void Tracker::move_servo(void)
-// {
-//   char out_buffer[100];
-//   for(pos = 0; pos < 180; pos += 20)  // goes from 0 degrees to 180 degrees
-//   {
-//     itoa(pos, out_buffer, 10);                      // in steps of 1 degree
-//     debug->log(out_buffer);
-//     servo->write(pos);            // tell servo to go to position in variable 'pos'
-//   }
-//   for(pos = 180; pos>=1; pos-=20)     // goes from 180 degrees to 0 degrees
-//   {
-//     itoa(pos, out_buffer, 10);                      // in steps of 1 degree
-//     debug->log(out_buffer);
-//     servo->write(pos);          // tell servo to go to position in variable 'pos'
-//   }
-// }
+void Tracker::move_servo(Sensor *sensor, int direction)
+{
+  move_servo(sensor, direction, 1);
+}
 
-void Tracker::toggle_laser()
+
+void Tracker::move_servo(Sensor *sensor, int direction, int amount)
+{
+  if (direction == SERVO_LEFT && sensor->servo_pos > 0)
+  {
+    sensor->servo_pos -= amount;
+    debug->log("Moving servo left");
+  }
+  else if (direction == SERVO_RIGHT && sensor->servo_pos < 180)
+  {
+    sensor->servo_pos += amount;
+    debug->log("Moving servo right");
+  }
+
+  sensor->servo->write(sensor->servo_pos);
+}
+
+void Tracker::execute(void)
+{
+  int delta;
+  if (edge_mode == RIGHT_EDGE)
+  {
+    delta = make_reading(right_sensor);
+  }
+  else if (edge_mode == LEFT_EDGE)
+  {
+    delta = make_reading(left_sensor);
+  }
+
+  if (delta < reading_threshold)
+  {
+    move_servo(left_sensor, left_sensor->servo_direction);
+    // TODO: add right sensor
+    if (left_sensor->servo_pos <= SERVO_MIN_POS || left_sensor->servo_pos >= SERVO_MAX_POS)
+    {
+      reverse_servo(left_sensor);
+    }
+  }
+  else // hit target
+  {
+    left_sensor->last_found_position = left_sensor->servo_pos;
+    if (left_sensor, edge_mode == LEFT_EDGE)
+    {
+      move_servo(left_sensor, SERVO_LEFT);
+      left_sensor->servo_direction = SERVO_RIGHT;
+    }
+    // TODO: add both edges
+  }
+  debug->log("Delta: %d\tServo Pos: %d", delta, left_sensor->servo_pos);
+}
+
+void Tracker::reverse_servo(Sensor *sensor)
+{
+  if (sensor->servo_direction == SERVO_RIGHT)
+  {
+    sensor->servo_direction = SERVO_LEFT;
+  }
+  else if (sensor->servo_direction == SERVO_LEFT)
+  {
+    sensor->servo_direction = SERVO_RIGHT;
+  }
+}
+
+int Tracker::make_reading(Sensor *sensor)
 {
   int laser_value = digitalRead(laser_pin);
   int reading = analogRead(transistor_pin);
-  int delta = reading - last_transistor_reading;
-  last_transistor_reading = reading;
+  int delta = reading - sensor->last_reading;
+  sensor->last_reading = reading;
   // laser is off, invert the delta
   if(laser_value == LOW)
   {
     delta *= -1;
   }
-  else
-  {
-  }
-  debug->log("%d", delta);
   digitalWrite(laser_pin, !laser_value);
+  return delta;
 }
 
-void Tracker::toggle_laser_wrapper(void *self)
+void Tracker::execute_wrapper(void *self)
 {
-  ((Tracker *)self)->toggle_laser();
+  ((Tracker *)self)->execute();
 }
--- Drone/tracker.h
+++ Drone/tracker.h
@@ -12,6 +12,12 @@
 #define ONE_EDGE 0
 #define TWO_EDGE 1
 
+#define SERVO_LEFT 0
+#define SERVO_RIGHT 1
+
+#define SERVO_MIN_POS 15
+#define SERVO_MAX_POS 165
+
 class Tracker
 {
   public:
@@ -19,21 +25,39 @@ class Tracker
     Tracker(int _mode, int _edge_mode, int _laser_pin, int _transistor_pin);
     void loop(void);
   private:
+    typedef struct
+    {
+      Servo *servo;
+
+      int state;
+
+      int servo_pos;
+      int servo_direction;
+      // the threshold delta voltage in which the tracker will determine whether or not it is hitting a reflective object
+      int last_reading;
+      int last_found_position;
+    }Sensor;
+
     // void move_servo(void);
-    void toggle_laser(void);
-    static void toggle_laser_wrapper(void *self);
+    void execute(void);
+    static void execute_wrapper(void *self);
+    int make_reading(Sensor *sensor);
+    void move_servo(Sensor *sensor, int direction);
+    void move_servo(Sensor *sensor, int direction, int amount);
+    void reverse_servo(Sensor *sensor);
+
     // determines whether there is one laser tracking one edge or two lasers tracking both
     const int edge_mode;
     // determines which edge is being tracked
     const int edge;
+
     const int laser_pin;
     const int transistor_pin;
 
-    int pos;
-    Servo *servo;
+    int reading_threshold;
     Timer *laser_timer;
-
-    int last_transistor_reading;
+    Sensor *left_sensor;
+    Sensor *right_sensor;
 };
 
 #endif
